a0d064536a8a75c5f5969a7741cde7d428b5d5f6
felicityzhang <xz1574@nyu.edu>
2019-05-08 20:19:10 -0400

    styling for ArtistList

--- client/src/components/ShowArtists.jsx
+++ client/src/components/ShowArtists.jsx
@@ -3,17 +3,19 @@ import React, { Component } from 'react'
 export default class ShowArtists extends Component {
     render() {
         return (
-            <div>
+            <div className='listModel'>
                 { this.props.artists.map( artist => (
                     <React.Fragment key={ artist.id }>
-                        { console.log( 'photooooo:', artist.photo ) }
+                        <div className='modleItem'>
+                            <div className="modelContainer">
+                                <img className='ModelImg' src={ artist.photo } alt='' max-width='100%' height='350px' />
+                            </div>
+                            <div><h3>{ artist.name }</h3> </div>
+                            <div><h5>{ artist.price }</h5></div>
+                            <div><h5>{ artist.role }</h5></div>
+                            <div><h5>{ artist.description }</h5></div>
 
-                        <h3>{ artist.name }</h3>
-                        <h5>{ artist.price }</h5>
-                        <h5>{ artist.role }</h5>
-                        <img src={ artist.photo } alt='' width='80px' height='150px' />
-                        <h5>More Info</h5>
-
+                        </div>
                     </React.Fragment >
                 ) ) }
             </div>
